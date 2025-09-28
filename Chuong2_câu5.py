b5be9e678c760760869fe5f7c702ff39cc2f1017
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-09-28 18:28:01 +0700

    Buoi1-Chuong2

---
+++ Chuong2_câu5.py
@@ -0,0 +1,7 @@
+'''
+Trình bày các loại ghi chú trong Python.
+Python có các loại ghi chú sau:
+- Ghi chú dòng sử dụng dấu thăng #
+- Ghi chú đoạn sử dụng dấu nháy đơn 3 lần  hoặc dấu nháy kép 3 lần 
+- 
+'''
