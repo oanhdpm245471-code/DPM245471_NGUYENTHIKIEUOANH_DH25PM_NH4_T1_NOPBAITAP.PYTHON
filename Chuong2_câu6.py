77b1e8f862fdb703697769fb115a2928f5479cd8
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-09-29 09:00:01 +0700

    Buoi1_Chuong2

---
+++ Chuong2_câu6.py
@@ -0,0 +1,9 @@
+''' Trình bày ý nghĩa toán tử /, //, %, **, and, or, is.
+-Toán tử /: Chia thông thường, kết quả luôn là số thực 
+- Toán tử //: Chia lấy phần nguyên, kết quả là số nguyên nếu 2 toán hạng đều là số nguyên, ngược lại kết quả là số thực
+- Toán tử %: chia lấy phần dư
+- Toán tử **: lũy thừa
+- Toán tử and: trả về true nếu cả 2 toán hạng đúng, sai trả về false
+- Toán tử or: Trả về false khi cả 2 đều sai, đúng trả về true
+- Toán tử is: so sánh 2 toán hạng có cùng tham chiếu đến 1 đối tuọng hay không
+'''
---
+++ Chuong2_câu7.py
@@ -0,0 +1,6 @@
+'''
+Trình bày một số cách nhập dữ liệu từ bàn phím.
+- Sử dụng hàm input(): Dùng để nhập dữ liệu từ bàn phím, dữ liệu nhập vào sẽ được coi là chuỗi (string)
+- SỬ dụng hàm eval(): Dùng để nhập dữ liệu từ bàn phím, dữ liệu nhập vào sẽ được đánh giá theo kiểu dữ liệu tương ứng ( int, float, str, list ...)
+
+'''
